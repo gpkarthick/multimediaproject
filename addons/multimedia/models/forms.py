2dda16e3c315d2c966d271bebd9376b4dfbf1270
KarthickGP <karthick.gp@ajaxmediatech.com>
2022-03-31 23:17:46 +0530

    unwanted button removed and form development

--- addons/multimedia/models/forms.py
+++ addons/multimedia/models/forms.py
@@ -7,10 +7,13 @@ class PanForm(models.Model):
     _description = "Pan form"
     _order = 'id desc'
 
-    name = fields.Char(string='Pan No')
-    party_name = fields.Char(string='Party Name')
+    name = fields.Char(string='Pan Card No')
+    party_name = fields.Char(string='Name')
+    father_name = fields.Char(string='Father Name')
     mobile_no = fields.Char(string='Mobile No')
     address = fields.Text(string='Address')
+    mail_id = fields.Char(string='Mail ID')
+    dob = fields.Date(string='DOB')
     assigned_to_uid = fields.Many2one('res.users', string='Assigned to')
     seq_no = fields.Char(string='Sequence No', size=18, readonly=True, default=lambda self: _('New'))
     create_date = fields.Datetime(string='Created Date', readonly=True)
@@ -18,11 +21,11 @@ class PanForm(models.Model):
     write_uid = fields.Many2one('res.users', string='Last Modified by', readonly=True)
     create_uid = fields.Many2one('res.users', string='Created by', readonly=True)
     attachment = fields.Binary(string="Image", attachment=True)
-    aadhar_front = fields.Binary(string="Aadhar Front", attachment=True)
-    aadhar_back = fields.Binary(string="Aadhar Back", attachment=True)
-    pan_front = fields.Binary(string="Pan Front", attachment=True)
-    pan_back = fields.Binary(string="Pan Back", attachment=True)
-    form_type = fields.Selection([('new_pan', 'New Pan'), ('update_pan', 'Update Pan'), ('child_pan', 'Child Pan')], string='Type')
+    aadhar_front = fields.Binary(string="Aadhar Front", attachment=True, required=True)
+    aadhar_back = fields.Binary(string="Aadhar Back", attachment=True, required=True)
+    pan_front = fields.Binary(string="Pan Front", attachment=True, required=True)
+    pan_back = fields.Binary(string="Pan Back", attachment=True, required=True)
+    writtern_form = fields.Binary(string="Form", attachment=True, required=True)
     state = fields.Selection([('draft', 'Draft'), ('processing', 'Processing'), ('done', 'Done'), ('verified', 'Verified')], string='Status', readonly=True, default='draft')
 
     user_logo = fields.Binary("Company Image", related='create_uid.image_1920')
@@ -56,10 +59,6 @@ class PanForm(models.Model):
             raise UserError(_('Please enter the 10 Digit mobile Number.'))
         if not self.name:
             raise UserError(_('Please enter the PAN Number.'))
-        if not self.party_name:
-            raise UserError(_('Please enter the Party Name'))
-        if not self.address:
-            raise UserError(_('Please enter the Party Address'))
         self.write({'state': 'done'})
 
 class ResUsers(models.Model):
@@ -67,12 +66,9 @@ class ResUsers(models.Model):
 
     mail_id = fields.Char(string='Mail ID', required=True)
     mobile_no = fields.Char(string='Mobile No', required=True)
-    district_id = fields.Many2one('district.master', string='District', required=True)
-    village_id = fields.Many2one('village.master', string='Village', required=True)
-    user_type = fields.Selection(string='Status', selection=[('shop', 'shop'), ('employee', 'employee'), ('management', 'management')], required=True)
-
-
-
+    district_id = fields.Many2one('district.master', string='District')
+    village_id = fields.Many2one('village.master', string='Village')
+    user_type = fields.Selection(string='User type', selection=[('shop', 'shop'), ('employee', 'employee'), ('management', 'management')], required=True)
 
 class DistrictMaster(models.Model):
     _name = 'district.master'
